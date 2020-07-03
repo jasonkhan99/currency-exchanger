ac3119070c3e43969ff10fafa78f06cd892a16b1
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-07-03 10:08:37 -0700

    add export class exchange rate to currency-exchange.js and import in main

---
+++ src/currency-exchange.js
@@ -0,0 +1,18 @@
+export class ExchangeRate {
+
+  async getExchangeRate() {
+    try {
+      let response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`);
+      let jsonifiedResponse;
+      if (response.ok && response.status == 200) {
+        jsonifiedResponse = await response.json();
+      } else {
+        jsonifiedResponse = false;
+      }
+      return jsonifiedResponse;
+    } catch(error) {
+      return false;
+    }
+  }
+
+}
--- src/main.js
+++ src/main.js
@@ -2,4 +2,6 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+import { ExchangeRate } from './../src/currency-exchange.js';
+
 
