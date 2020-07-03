b340062ce8c09250326f45bfae4afe01c625e4d5
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-07-03 08:09:48 -0700

    create initial files, folders and webpack

--- src/main.js
+++ src/main.js
@@ -3,9 +3,3 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
-$(document).ready(function () {
-  // $('#submit-button').submit(function (event) {
-  //     event.preventDefault();
-
-  // });
-});
